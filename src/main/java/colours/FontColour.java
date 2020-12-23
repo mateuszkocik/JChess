7aad7e392a9bac58d5620bc6d712994d105e5354
Mateusz Kocik <mateusz.kocik9@gmail.com>
2020-12-23 13:52:30 +0100

    Add colours enums

---
+++ src/main/java/colours/FontColour.java
@@ -0,0 +1,23 @@
+package colours;
+
+public enum FontColour{
+    BLACK("\033[0;30m"),
+    RED("\033[0;31m"),
+    GREEN("\033[0;32m"),
+    YELLOW("\033[0;33m"),
+    BLUE("\033[0;34m"),
+    PURPLE("\033[0;35m"),
+    CYAN("\033[0;36m"),
+    WHITE("\033[0;37m");
+
+    String code;
+
+    FontColour(String code){
+        this.code = code;
+    }
+
+    @Override
+    public String toString(){
+        return code;
+    }
+}
