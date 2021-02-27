90b27feb9da9e4b28be11e90dfd3266f7f0fc314
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-02-27 19:09:53 +0100

    Add Piece class

---
+++ src/main/java/game/pieces/Piece.java
@@ -0,0 +1,52 @@
+package game.pieces;
+
+import game.Chessboard;
+import game.Coordinates;
+import game.Team;
+
+import java.util.List;
+
+public abstract class Piece{
+    private final PieceType type;
+    protected final Team team;
+    protected Coordinates coordinates;
+
+    public Piece(PieceType type, Team team, Coordinates coordinates){
+        this.type = type;
+        this.team = team;
+        this.coordinates = coordinates;
+    }
+
+    public Piece(PieceType type, Team team, char file, int rank){
+        this.type = type;
+        this.team = team;
+        this.coordinates = new Coordinates(file, rank);
+    }
+
+    public abstract List<Coordinates> getAvailableMoves(Chessboard board);
+
+    public Piece move(Coordinates to){
+        coordinates = to;
+        return this;
+    }
+
+    public Coordinates getCoordinates(){
+        return coordinates;
+    }
+
+    public PieceType getType(){
+        return type;
+    }
+
+    public String getName(){
+        return type.getName();
+    }
+
+    public int getWeight(){
+        return type.getWeight();
+    }
+
+    public Team getTeam(){
+        return team;
+    }
+}
