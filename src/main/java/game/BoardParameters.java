66044dbd3d111fd47df6ff4393694e27cc2a0262
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-02-27 19:10:24 +0100

    Add BoardParameters

---
+++ src/main/java/game/BoardParameters.java
@@ -0,0 +1,9 @@
+package game;
+
+public class BoardParameters{
+    public static final int SIZE = 8;
+    public static final char FROM_FILE = 'a';
+    public static final char TO_FILE = 'h';
+    public static final int FROM_RANK = 1;
+    public static final int TO_RANK = 8;
+}
