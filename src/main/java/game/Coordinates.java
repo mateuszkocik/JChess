41d15454c680e0f8ce7aed9b27e69fc9fc20940f
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-02-28 22:29:22 +0100

    Add equals and hashCode methods in Coordinates class

--- src/main/java/game/Coordinates.java
+++ src/main/java/game/Coordinates.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.Objects;
+
 import static game.BoardParameters.*;
 import static game.BoardParameters.TO_RANK;
 
@@ -50,14 +52,32 @@ public class Coordinates{
     }
 
     public Coordinates bottomLeft(){
-        return new Coordinates((char) (file -1), rank -1);
+        return new Coordinates((char) (file - 1), rank - 1);
     }
 
     public boolean isValid(){
         return file >= FROM_FILE && file <= TO_FILE && rank >= FROM_RANK && rank <= TO_RANK;
     }
 
-    public static Coordinates make(char file, int rank){
-        return new Coordinates(file,rank);
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return file == that.file && rank == that.rank;
     }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(file, rank);
+    }
+
+    @Override
+    public String toString(){
+        return "Coordinates{" +
+                "file=" + file +
+                ", rank=" + rank +
+                '}';
+    }
+
 }
