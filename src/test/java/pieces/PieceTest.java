91d014774259234ff2f5bfd611b2e55e4c66bd8c
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-03-01 23:24:04 +0100

    Add PieceTest

---
+++ src/test/java/pieces/PieceTest.java
@@ -0,0 +1,57 @@
+package pieces;
+
+import game.Chessboard;
+import game.Coordinates;
+import game.pieces.Piece;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static game.Coordinates.makeCoord;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public abstract class PieceTest{
+
+    protected Chessboard chessboard;
+
+    @BeforeEach
+    void setUp(){
+        chessboard = new Chessboard();
+    }
+
+    protected void emptyAvailableMoves(Piece piece){
+        assertEquals(piece.getAvailableMoves(chessboard).size(), 0);
+    }
+
+    protected void pieceAttackMovesEqualsStringCoords(Piece piece, String coords){
+        var stringCoords = stringToCoordinates(coords);
+        var pieceAttackMoves = piece.getAttackMoves(chessboard);
+        coordinatesListAreTheSameIgnoringOrder(stringCoords, pieceAttackMoves);
+    }
+
+    protected void pieceAvailableMovesEqualsStringCoords(Piece piece, String coords){
+        var stringCoords = stringToCoordinates(coords);
+        var pieceAvailableMoves = piece.getAvailableMoves(chessboard);
+        coordinatesListAreTheSameIgnoringOrder(stringCoords, pieceAvailableMoves);
+    }
+
+    private void coordinatesListAreTheSameIgnoringOrder(List<Coordinates> l1, List<Coordinates> l2){
+        assertTrue(l1.size() == l2.size() && l2.containsAll(l1) && l1.containsAll(l2));
+    }
+
+    private List<Coordinates> stringToCoordinates(String coords){
+        var result = coords.split(",");
+        return Arrays
+                .stream(result)
+                .map(s -> makeCoord(s.charAt(0), s.charAt(1) - '0'))
+                .collect(Collectors.toList());
+    }
+
+    protected void setPiece(Piece piece){
+        chessboard.set(piece);
+    }
+
+}
