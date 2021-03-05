d5a6f90430f97c35ef545cb3e63beb73119b0936
Mateusz Kocik <mateusz.kocik9@gmail.com>
2021-03-05 23:02:35 +0100

    Add Move class

---
+++ src/main/java/game/Move.java
@@ -0,0 +1,20 @@
+package game;
+
+public class Move{
+
+    private final Coordinates from;
+    private final Coordinates to;
+
+    public Move(Coordinates from, Coordinates to){
+        this.from = from;
+        this.to = to;
+    }
+
+    public Coordinates getFrom(){
+        return from;
+    }
+
+    public Coordinates getTo(){
+        return to;
+    }
+}
